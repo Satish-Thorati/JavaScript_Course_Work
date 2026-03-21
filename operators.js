7855403216806c75ac00f776b919a2a985f0f11b
Satish-Thorati <satishthorati09@gmail.com>
2026-03-21 09:47:23 +0530

    Operators

---
+++ operators.js
@@ -0,0 +1,76 @@
+//Comparison Operators
+let num = 20 >= 20;
+console.log(num)
+
+let value_1 = 12 * 2 > 31;
+console.log(value_1)
+
+let value_2 = 22 * 2 < 55;
+console.log(value_2)
+
+let value_3 = 6 * 3 <= 31;
+console.log(value_3)
+
+let strict_equal = 5 * 5 === "25"; // checks value and type
+console.log(strict_equal)
+
+let loose_equal = 8 * 8 == "64"; //checks only value
+console.log(strict_equal)
+
+let not_equal = 9 * 9 != 81;
+console.log(not_equal)
+
+let not_strick_equal = 4 * 2 !== "21";
+console.log(not_strick_equal)
+
+//Logical Operators
+//LOGICAL AND
+let logical_and = 7 * 3 == "21" && 5 + 5 ==10;
+console.log(logical_and)
+
+let val_4 = false && false;
+console.log(val_4)
+
+let val_5 = true && true;
+console.log(val_5)
+
+let val_6 = true && false;
+console.log(val_6)
+
+let val_7 = false && true;
+console.log(val_7)
+
+//LOGICAL OR
+let logical_or = 5 + 6 == 11 || 9 * 8 == 81;
+console.log(logical_or)
+
+let val_8 = true || true;
+console.log(val_8)
+
+let val_9 = false || false;
+console.log(val_9)
+
+let val_10 = true || false;
+console.log(val_10)
+
+//LOGICAL NOT 
+let logical_not = !false;
+console.log(logical_not)
+
+let logical_not_1 = !true;
+console.log(logical_not_1)
+
+let result = 3 * 7 !== 21 && 9 + 20 === "29" || 20 * 10 == 200
+console.log(result)
+
+let result_1 = 3 * 7 !== 21 && 9 + 20 === "29" || !20 * 10 == 200 && 12 + 6 === 18;
+console.log(result_1)
+
+
+
+
+
+
+
+
+

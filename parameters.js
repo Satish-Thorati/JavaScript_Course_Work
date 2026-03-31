35b2e80b3d8c729358f04103d15f3e509d362254
Satish-Thorati <satishthorati09@gmail.com>
2026-03-31 09:33:34 +0530

    parameters

---
+++ parameters.js
@@ -0,0 +1,10 @@
+//parameters -- storing the values and reuseble 
+function sum(a,b){//(a,b) are parameters
+    console.log(a - b)
+    console.log(a + b)
+}
+sum(20,40) //agrument the values
+sum(20)
+sum()
+
+//NaN --it is a datatype, if you are not passing the arguments it gives NaN value as output

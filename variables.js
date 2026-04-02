d168e05504a40d87589b00921039cbcd8d478596
Satish-Thorati <satishthorati09@gmail.com>
2026-04-02 09:58:05 +0530

    js_variabless

--- variables.js
+++ variables.js
@@ -1,25 +1,24 @@
 // //let -- reassign, but not redeclare
-// let value=10;
-// value=40;
-// value +=50;
-// value -= 20;
-// value /= 20;
+let value=10;
+value=40;
+value +=50;
+value -= 20;
+value /= 20;
+//let value=120;  -- not redeclare
 
-// //let value=120;
+console.log(value);
 
-// console.log(value);
+//const -- not reassign, not redeclare
+const num=20;
+//num=60;
+//const num=30;
+console.log(num);
 
-// //const -- not reassign, not redeclare
-// const num=20;
-// //num=60;
-// //const num=30;
-// console.log(num);
-
-// //var -- reassign and redeclare
-// var add=30;
-// add=80;
-// var add=120;
-// console.log(add)
+//var -- reassign and redeclare
+var add=30;
+add=80;
+var add=120;
+console.log(add)
 
 let product=12;
 product *=12;
